db20bf56cc77aed430dd4f9d8e726e5ebf3d968a
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-07-20 13:31:38 +0700

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int n,k,a[1005],final=0;
+void ktao(){
+    cin>>n>>k;
+    for(int i=1;i<=k;i++){
+        a[i]=i;
+    }
+}
+void sinh(){
+    int i=k;
+    while(i>=1&&a[i]==n-k+i){
+        i--;
+    }
+    if(i==0) final=1;
+    else{
+        a[i]++;
+        for(int j=i+1;j<=k;j++){
+            a[j]=a[j-1]+1;
+        }
+    }
+}
+int main(){
+    ktao();
+    while(final==0){
+        for(int i=1;i<=k;i++){
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
+        sinh();
+    }
+}
