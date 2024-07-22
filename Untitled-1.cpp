adbe6f9bed798994b984239194881f6cfb2cda99
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-07-22 22:47:40 +0700

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n,a[1005],final=0;
+void ktao(){
+    cin>>n;
+    for(int i=1;i<=n;i++) {
+        a[i]=n-i+1;
+    }
+}
+void sinh(){
+    int i=n-1;
+    while(i>=1&&a[i+1]>a[i]){
+        i--;
+    }
+    if(i==0) final=1;
+     else{
+        int j=n;
+        while(a[j]>a[i])j--;
+        swap(a[i],a[j]);
+        reverse(a+i+1,a+n+1);
+    }
+}
+int main() {
+    int t;cin>>t;
+    while(t--){
+        final=0;
+        ktao();
+        while(final==0){
+            for(int i=1;i<=n;i++) {
+                cout<<a[i];
+            }
+            cout<<" ";
+            sinh();
+        }
+        cout<<endl;
+    }
+}
