1da783ace615e76cc0b31349ce945fbc171196b0
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-08-02 11:32:39 +0700

    Add files via upload

---
+++ CODE_PTIT.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int x[100], n;
+void in()
+{
+   for (int i = 1; i <= n; i++)
+   {
+      cout << x[i];
+   }
+}
+void Try(int i)
+{
+   for (int j = 0; j <= 1; j++)
+   {
+      x[i] = j;
+      if (i == n)
+      {
+         in();
+         cout << endl;
+      }
+      else
+         Try(i + 1);
+   }
+}
+int main()
+{
+   cin >> n;
+   Try(1);
+}
