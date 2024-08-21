22e92eac6d0473b7328c3422d69797b526d3adb6
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-08-21 21:16:28 +0700

    Update DSA01015 - TÌM BỘI SỐ.cpp

--- DSA01015 - TÌM BỘI SỐ.cpp
+++ DSA01015 - TÌM BỘI SỐ.cpp
@@ -21,39 +21,31 @@ int main(){
         cout<<solve(n)<<endl;
     }
 }
-C2:#include <bits/stdc++.h>
+C2:#include<bits/stdc++.h>
 using namespace std;
-void check()
-{
-   int n;
-   cin >> n;
+void check(){
+   int n;cin>>n;
    queue<string> q;
    q.push("9");
-   while (true)
-   {
-      string s = q.front();
+   while(true){
+      string s=q.front();
       q.pop();
-      int x = 0;
-      for (char i : s)
-      {
-         x = x * 10 + (i - '0');
-         x %= n;
+      int x=0;
+      for(char i:s){
+         x=x*10+(i-'0');
+         x%=n;
       }
-      if (x == 0)
-      {
-         cout << s << endl;
+      if(x==0){
+         cout<<s<<endl;
          return;
       }
-      q.push(s + '0');
-      q.push(s + '9');
+      q.push(s+'0');
+      q.push(s+'9');
    }
 }
-int main()
-{
-   int t;
-   cin >> t;
-   while (t--)
-   {
+int main(){
+   int t;cin>>t;
+   while(t--){
       check();
    }
 }
