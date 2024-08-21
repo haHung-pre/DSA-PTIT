d5977eaa22f5d367fde0ae344118edff7b5fff8a
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-08-21 21:14:12 +0700

    Update and rename DSA01015 - TÌM BỘI SỐ to DSA01015 - TÌM BỘI SỐ.cpp

--- DSA01015 - TÌM BỘI SỐ.cpp
+++ DSA01015 - TÌM BỘI SỐ.cpp
@@ -57,29 +57,23 @@ int main()
       check();
    }
 }
-C3:#include <bits/stdc++.h>
+C3:#include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-   int t;
-   cin >> t;
-   while (t--)
-   {
-      int n;
-      cin >> n;
-      queue<long long> q;
+int main(){
+   int t;cin>>t;
+   while(t--){
+      int n;cin>>n;
+      queue<long long>q;
       q.push(9);
-      while (q.size())
-      {
-         long long k = q.front();
+      while(q.size()){
+         long long k=q.front();
          q.pop();
-         if (k % n == 0)
-         {
-            cout << k << endl;
+         if(k%n==0){
+            cout<<k<<endl;
             break;
          }
-         q.push(k * 10);
-         q.push(k * 10 + 9);
+         q.push(k*10);
+         q.push(k*10+9);
       }
    }
 }
