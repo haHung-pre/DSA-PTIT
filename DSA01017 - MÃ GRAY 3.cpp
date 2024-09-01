3bca25c0d59d4635a4a132fb79d32eedaf9a6296
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-09-01 15:50:10 +0700

    Update and rename DSA01017 - MÃ GRAY 3 to DSA01017 - MÃ GRAY 3.cpp

--- DSA01017 - MÃ GRAY 3.cpp
+++ DSA01017 - MÃ GRAY 3.cpp
@@ -1,4 +1,24 @@
-#include<iostream>
+C1:#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    while(t--){
+        string s;cin>>s;
+    	string res(1,s[0]);
+    	for(int i=1;i<s.length();i++){
+        if(s[i]!=s[i-1])
+            res.push_back('1');
+        else
+            res.push_back('0');
+    }
+    cout<<res;
+        cout<<endl;
+    }
+}
+
+
+
+C2:#include<iostream>
 #include<vector>
 #define pb push_back
 using namespace std;
