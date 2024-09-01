6a17491633bdbdfbb658781485f5bd613ea753e0
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-09-01 15:37:27 +0700

    Update DSA06045 - XÂU TỐT.cpp

--- DSA06045 - XÂU TỐT.cpp
+++ DSA06045 - XÂU TỐT.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h> 
+C1:#include<bits/stdc++.h> 
 using namespace std;
 int main(){
     int n;string s;
@@ -16,3 +16,28 @@ int main(){
     cout<<res<<endl;
 }
 
+
+
+
+
+C2:#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;string s;
+    cin>>n>>s;
+    long long total=(1LL*n*(n - 1))/2;
+    int c=1;
+    vector<int>v;
+    for(int i=1;i<s.size();i++){
+        if(s[i]==s[i - 1])
+            c++;
+        else{
+            v.push_back(c);
+            c=1;
+        }
+    }
+    v.push_back(c);
+    for(int i=1;i<v.size();i++)
+        total-=(v[i]+v[i-1]-1);
+    cout<<total;
+}
