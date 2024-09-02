811157e50ae1ec32de5466945c79e71af5012e54
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-09-02 18:01:39 +0700

    Update CTDL_001 - THUẬT TOÁN SINH.cpp

--- CTDL_001 - THUẬT TOÁN SINH.cpp
+++ CTDL_001 - THUẬT TOÁN SINH.cpp
@@ -1,48 +1,45 @@
-C1:#include<iostream>
+C1:#include<bits/stdc++.h>
 using namespace std;
-int n,a[1005],final=0;
+int n,a[20],final=0,ok=0;
 void ktao(){
-   for(int i=1;i<=n;i++){
-      a[i]=0;
-   }
+	for(int i=1;i<=n;i++){
+		a[i]=0;
+	}
 }
 void sinh(){
-   int i=n;
-   while(i>=1&&a[i]==1){
-      a[i]=0;
-      i--;
-   }
-   if(i==0) final=1;
-   else{
-      a[i]=1;
-   }
+	int i=n;
+	while(i>0&&a[i]==1){
+		a[i]=0;
+		i--;
+	}
+	if(i==0)final=1;
+	else{
+		a[i]=1;
+	}
 }
-bool check(){
-   int l=0,r=n+1;
-   while(l<=r){
-      if(a[l++]!=a[r--]){
-         return false;
-      }
-   }
-   return true;
-}
-void out(){
-   for(int i=1;i<=n;i++){
-      cout<<a[i]<<" ";
-   }
-   cout<<endl;
+int check(){
+	int l=0,r=n+1;
+	while(l<r){
+		if(a[l]!=a[r])return 0;
+		l++;r--;
+	}
+	return 1;
 }
 int main(){
-   cin>>n;
-   ktao();
-   final=0;
-   while(final==0){
-      if(check()){
-         out();
-      }
-      sinh();
-   }
-}
+	cin>>n;
+	ktao();
+	while(final==0){
+		if(check()){
+		for(int i=1;i<=n;i++){
+			cout<<a[i]<<" ";
+		}
+		cout<<endl;
+	}
+	sinh();
+	}
+} 
+
+
 
 
 
