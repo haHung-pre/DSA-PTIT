53db65840ad0ff30c71fd147f3eedf810479562c
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-09-06 00:05:34 +0700

    Update and rename DSA02024 - DÃY CON TĂNG DẦN to DSA02024 - DÃY CON TĂ.cpp

---
+++ DSA02024 - DÃY CON TĂ.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,x[100],a[100];
+vector<string>v;
+void ql(int i,int bd){
+   for(int j=bd;j<=n;j++){
+      if(a[j]>x[i-1]){
+         x[i]=a[j];
+         if(i>=2){
+            string s="";
+            for(int u=1;u<=i;u++){
+               s+=to_string(x[u])+" ";
+            }
+            v.push_back(s);
+         }
+         ql(i+1,j+1);
+      }
+   }
+}
+int main(){
+   cin>>n;
+   for(int i=1;i<=n;i++){
+      cin>>a[i];
+   }
+   x[0]=0;
+   ql(1,1);
+   sort(v.begin(),v.end());
+   for(auto x:v)
+      cout<<x<<endl;
+}
