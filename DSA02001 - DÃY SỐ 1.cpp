06127395f057fff2ab39d49ec18128d2a635a683
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-09-05 22:54:51 +0700

    Update and rename DSA02001 - DÃY SỐ 1 to DSA02001 - DÃY SỐ 1.cpp

--- DSA02001 - DÃY SỐ 1.cpp
+++ DSA02001 - DÃY SỐ 1.cpp
@@ -1,42 +1,28 @@
-NOTICE:#include <bits/stdc++.h>
+NOTICE:#include<bits/stdc++.h>
 using namespace std;
-
 int n, a[100];
-
-void ql(int i)
-{
-   if (n - i + 1 <= 0)
+void ql(int i){
+   if (n-i+1<= 0)
       return;
-
    cout << "[";
-   for (int j = 1; j <= n - i + 1; j++)
-   {
-      cout << a[j];
-      if (j < n - i + 1)
-         cout << " ";
+   for (int j = 1;j<=n-i+1;j++){
+      cout<<a[j];
+      if (j<n-i+1)
+         cout<<" ";
    }
-   cout << "]" << endl;
-
-   for (int j = 1; j < n - i + 1; j++)
-   {
-      a[j] = a[j] + a[j + 1];
+   cout<< "]"<<endl;
+   for (int j=1;j<n-i+1;j++){
+      a[j]=a[j]+a[j + 1];
    }
-
    ql(i + 1);
 }
-
-int main()
-{
-   int t;
-   cin >> t;
-   while (t--)
-   {
-      cin >> n;
-      for (int i = 1; i <= n; i++)
-      {
-         cin >> a[i];
+int main(){
+   int t;cin>>t;
+   while(t--){
+      cin>>n;
+      for(int i = 1; i <= n; i++){
+         cin>>a[i];
       }
-
       ql(1);
    }
 }
