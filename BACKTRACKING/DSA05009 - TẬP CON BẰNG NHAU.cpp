7898cbd74fcbb3a9cf836f5cb82db80a1d4da710
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-09-13 21:45:48 +0700

    Update DSA05009 - TẬP CON BẰNG NHAU.cpp

--- BACKTRACKING/DSA05009 - TẬP CON BẰNG NHAU.cpp
+++ BACKTRACKING/DSA05009 - TẬP CON BẰNG NHAU.cpp
@@ -1,53 +1,42 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int n, s = 0, a[102], x[102], ok = 0;
-void ql(int i, int bd, int sum)
-{
-   if (ok)
+int n,s=0,a[102],x[102],ok=0;
+void ql(int i,int bd,int sum){
+   if(ok)
       return;
-   for (int j = bd; j <= n; j++)
-   {
-      if (sum + a[j] <= s)
-      {
-         x[i] = a[j];
-         if (sum + a[j] == s)
-         {
-            ok = 1;
+   for(int j=bd;j<=n;j++){
+      if(sum+a[j]<=s){
+         x[i]=a[j];
+         if(sum+a[j]==s){
+            ok=1;
          }
          else
-            ql(i + 1, j + 1, sum + a[j]);
+            ql(i+1,j+1,sum+a[j]);
       }
    }
 }
-int main()
-{
-   int t;
-   cin >> t;
-   while (t--)
-   {
-      s = 0;
-      ok = 0;
-      cin >> n;
-      for (int i = 1; i <= n; i++)
-      {
-         cin >> a[i];
-         s += a[i];
+int main(){
+   int t;cin>>t;
+   while(t--){
+      s=0;
+      ok=0;
+      cin>>n;
+      for(int i=1;i<=n;i++){
+         cin>>a[i];
+         s+=a[i];
       }
-      if (s % 2 == 1)
-      {
-         cout << "NO\n";
+      if(s%2==1){
+         cout<<"NO\n";
          return 0;
       }
-      s /= 2;
-      sort(a + 1, a + n + 1);
-      ql(1, 1, 0);
-      if (ok == 0)
-      {
-         cout << "NO\n";
+      s/=2;
+      sort(a+1,a+n+1);
+      ql(1,1,0);
+      if(ok==0){
+         cout<<"NO\n";
       }
-      else
-      {
-         cout << "YES\n";
+      else{
+         cout<<"YES\n";
       }
    }
 }
