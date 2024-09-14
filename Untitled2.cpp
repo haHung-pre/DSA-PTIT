7345f957d9fe3da6436cc207a6574145fd6239c3
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-09-14 09:15:22 +0700

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;cin>>t;
+	while(t--){
+		int n;cin>>n;
+		int x,s=0,mod=1e9+7;
+		priority_queue<int,vector<int>,greater<int>>q;
+		for(int i=0;i<n;i++){
+			cin>>x;
+			q.push(x);
+		}
+		while(q.size()>1){
+			int s1=q.top();
+			q.pop();
+			int s2=q.top();
+			q.pop();
+			int k=(s1 + s2)%mod;
+			s=(s+k)%mod;
+			q.push(k);
+		}
+		cout<<s<<endl;
+	}
+}
