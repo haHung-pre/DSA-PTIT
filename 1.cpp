ca00b4a697f94a085a1f92087797749003c328aa
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-09-14 11:26:44 +0700

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;cin>>n;
+    int a[n];
+    for(int &i:a)cin>>i;
+    sort(a,a+n);
+    int ans=max({a[0]*a[1],a[n-1]*a[n-2]*a[n-3],a[n-1]*a[n-2],
+	a[0]*a[1]*a[n-1]});
+    cout<<ans;
+}
