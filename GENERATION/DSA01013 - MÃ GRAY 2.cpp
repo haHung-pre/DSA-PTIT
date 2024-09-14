b011cc78e2ec148b60c7f14ba3e53cfcde459595
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-09-14 08:58:46 +0700

    Update and rename DSA01013 - MÃ GRAY 2.cpp to GENERATION/DSA01013 - MÃ GRAY 2.cpp

--- GENERATION/DSA01013 - MÃ GRAY 2.cpp
+++ GENERATION/DSA01013 - MÃ GRAY 2.cpp
@@ -1,4 +1,26 @@
-#include<iostream>
+c1:#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;cin>>t;
+	while(t--){
+		string s;cin>>s;
+		for(int i=1;i<s.size();i++){
+			s[i]=((s[i]-'0')^(s[i-1]-'0'))+'0';
+		}
+		cout<<s<<endl;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+c2:#include<iostream>
 using namespace std;
 int main(){
     int t;cin>>t;
