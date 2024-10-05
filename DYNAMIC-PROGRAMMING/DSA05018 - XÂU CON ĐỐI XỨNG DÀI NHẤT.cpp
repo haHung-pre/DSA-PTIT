660a7d8702103c260697e20a59576c28fa89fcb6
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-10-05 08:38:40 +0700

    Update DSA05018 - XÂU CON ĐỐI XỨNG DÀI NHẤT.cpp

--- DYNAMIC-PROGRAMMING/DSA05018 - XÂU CON ĐỐI XỨNG DÀI NHẤT.cpp
+++ DYNAMIC-PROGRAMMING/DSA05018 - XÂU CON ĐỐI XỨNG DÀI NHẤT.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+C1:#include<bits/stdc++.h>
 using namespace std;
 int main(){
 	int t;cin>>t;
@@ -17,4 +17,41 @@ int main(){
 			}
 		cout<<ans<<endl;
 	}
+}
+
+
+
+
+
+
+
+
+
+
+C2:#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    while(t--){
+        string s;cin>>s;
+        int n=s.size();
+        int l,r,ans=0;
+        for(int i=0;i<n;i++){
+            l=i-1;
+            r=i+1;
+            while(l>=0&&r<n&&s[l]==s[r]){
+                l--;
+                r++;
+            }
+            ans=max(ans,r-l-1);
+            l=i-1;
+            r=i;
+            while(l>=0&&r<n&&s[l]==s[r]){
+                l--;
+                r++;
+            }
+            ans=max(ans,r-l-1);
+        } 
+        cout<<ans<<endl;
+    }
 }
