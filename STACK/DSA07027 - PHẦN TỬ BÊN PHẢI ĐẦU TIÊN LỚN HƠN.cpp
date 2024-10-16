91601e5566ad810b2e1e07b1f075f2263c38ea57
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-10-16 22:02:20 +0700

    Update DSA07027 - PHẦN TỬ BÊN PHẢI ĐẦU TIÊN LỚN HƠN.cpp

--- STACK/DSA07027 - PHẦN TỬ BÊN PHẢI ĐẦU TIÊN LỚN HƠN.cpp
+++ STACK/DSA07027 - PHẦN TỬ BÊN PHẢI ĐẦU TIÊN LỚN HƠN.cpp
@@ -1,28 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	int t;cin>>t;
-	while(t--){
-		int n;cin>>n;
-		int a[n],b[n];
-		for(int i=0;i<n;i++){
-			cin>>a[i];
-		}
-		stack<int>st;
-		for(int i=0;i<n;i++){
-				while(!st.empty()&&a[st.top()]<a[i]){
-					b[st.top()]=a[i];
-					st.pop();
-				}
-				st.push(i);
-		}		
-		while(!st.empty()){
-			b[st.top()]=-1;
-			st.pop();
-		}
-		for(int i=0;i<n;i++){
-			cout<<b[i]<<" ";
-		}
-		cout<<endl;
+    int t;cin>>t;
+    while(t--){
+    int n;cin>>n;
+    vector<int>a(n),r(n);
+    for(int &i:a)cin>>i;
+    stack<int>st;
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty()&&st.top()<=a[i]){
+            st.pop();
+        }
+        if(st.empty())r[i]=-1;
+        else r[i]=st.top();
+        st.push(a[i]);
+    }
+    for(int i:r)cout<<i<<" ";
+    cout<<endl;
 	}	
 }
