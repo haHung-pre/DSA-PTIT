0c38b8513a5f8c2cdb7f96e2b8df60c3d72f13b6
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-10-31 22:43:51 +0700

    Update DSA02006 - DÃY CON TỔNG BẰNG K.cpp

--- BACKTRACKING/DSA02006 - DÃY CON TỔNG BẰNG K.cpp
+++ BACKTRACKING/DSA02006 - DÃY CON TỔNG BẰNG K.cpp
@@ -1,32 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,k,a[105],x[105];
+int a[500],n,s,x[500];
+bool check;
 void ql(int i,int bd,int sum){
-   for(int j=bd;j<=n;j++){
-      x[i]=a[j];
-      if(sum+a[j]==k){
-         cout<<"[";
-         for(int u=1;u<=i;u++){
-            cout<<x[u];
-            if(u<i)
-               cout<<" ";
-         }
-         cout<<"]";
-         cout<<" ";
-      }
-      else if(sum+a[j]<k)
-         ql(i+1,j+1,sum+a[j]);
-   }
+	for(int j=bd;j<=n;j++){
+	x[i]=a[j];		
+	if(sum+a[j]==s){
+		check=true;
+		cout<<"[";
+		for(int u=1;u<=i;u++){
+			cout<<x[u];
+			if(u<i)cout<<" ";
+			if(u==i)cout<<"]";
+		}
+		cout<<" ";
+	}
+	else if(sum+a[j]<s)
+	ql(i+1,j+1,sum+a[j]);
+}
 }
 int main(){
-   int t;cin>>t;
-   while(t--){
-      cin>>n>>k;
-      for(int i=1;i<=n;i++){
-         cin>>a[i];
-      }
-      sort(a+1,a+n+1);
-      ql(1,1,0);
-      cout<<endl;
-   }
+	int t;cin>>t;
+	while(t--){
+		check=false;
+		cin>>n>>s;
+		for(int i=1;i<=n;i++)cin>>a[i];
+		sort(a+1,a+n+1);
+		ql(1,1,0);
+		if(check==false)cout<<"-1"; 
+		cout<<endl;
+	}
 }
