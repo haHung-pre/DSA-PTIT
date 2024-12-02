9dd1ebb5384201c721abc256b156e8b5b16fc949
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-12-02 17:19:48 +0700

    Update DSA01019 - HAHAHA.cpp

--- GENERATION/DSA01019 - HAHAHA.cpp
+++ GENERATION/DSA01019 - HAHAHA.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
-#include<queue>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
-int check(string s){
-    if(s[0]!='H'||s[s.length()-1]!='A')return 0;
-    for(int i=0;i<s.length()-1;i++){
-        if(s[i]=='H'&&s[i+1]=='H')
+bool check(string s){
+    int n=s.size();
+    if(s[0]!='H'||s[n-1]!='A')
+        return 0;
+    for(int i=0;i<n-1; i++){
+        if (s[i]=='H'&&s[i+1]=='H')
             return 0;
     }
     return 1;
@@ -14,19 +14,24 @@ int main(){
     int t;cin>>t;
     while(t--){
         int n;cin>>n;
-        string a="H",b="A";
-        queue<string>q;
-        q.push(a),q.push(b);
-        vector<string>v;
+        string s;
+        for(int i=0;i<n;i++)
+            s+='A';
         while(1){
-            string top=q.front();q.pop();
-            if(top.size()>n)break;
-            if(top.size()==n&&check(top))
-                v.push_back(top);
-            q.push(top + a);
-            q.push(top + b);
+            if(check(s))
+                cout<<s<<endl;
+            int ok=0;
+            for(int i=n-1;i>=0;i--){
+                if(s[i]=='A'){
+                    ok=1;
+                    s[i]='H';
+                    for (int j=i+1;j<n;j++)
+                        s[j]='A';
+                    break;
+                }
+            }
+            if(ok==0)
+                break;
         }
-        sort(v.begin(),v.end());
-        for(string x:v)cout<<x<<endl;
     }
 }
