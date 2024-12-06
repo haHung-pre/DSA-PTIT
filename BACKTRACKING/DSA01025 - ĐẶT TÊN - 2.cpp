141021b000471c117387ec2f5ccf4309d4132a4d
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2024-12-06 23:00:40 +0700

    Update DSA01025 - ĐẶT TÊN - 2.cpp

--- BACKTRACKING/DSA01025 - ĐẶT TÊN - 2.cpp
+++ BACKTRACKING/DSA01025 - ĐẶT TÊN - 2.cpp
@@ -1,74 +1,22 @@
-C1:#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
-#define pb push_back
-int n,k,x[100],ok;
-vector<char>v;
-void init(){
-    v.clear();
-    ok=1;
-    cin>>n>>k;
-    v.pb('@');
-    for(int i=1;i<=n;i++){
-        v.pb(i+64);
-        x[i]=i;
-    }
-}
-void next(){
-    int i=k;
-    while(i>0&&x[i]==n-k+i)i--;
-    if(!i)ok=0;
-    else{
-        x[i]++;
-        for(int j=i+1;j<=k;j++)
-            x[j]=x[j-1]+1;
-    }
-}
-void out(){
+int x[15],n,k;
+void in(){
     for(int i=1;i<=k;i++)
-        cout<<v[x[i]];
+        cout<<char('A'+x[i]-1);
     cout<<endl;
+}
+void ql(int i,int bd){
+    for(int j=bd+1;j<=n-k+i;j++){
+        x[i]=j;
+        if(i==k)in();
+        else ql(i+1,j);
+    }
 }
 int main(){
     int t;cin>>t;
     while(t--){
-        init();
-        while(ok){
-            out();
-            next();
-        }
+        cin>>n>>k;
+        ql(1,0);
     }
-}
-
-
-
-
-C2:#include<bits/stdc++.h>
-using namespace std;
-int n,k;
-vector<char>letters;
-void ql(vector<char>&s,int bd){
-   if(s.size()==k){
-      for(char c:s)
-         cout<<c;
-      cout<<endl;
-      return;
-   }
-   for(int i=bd;i<n;i++){
-      s.push_back(letters[i]);
-      ql(s,i+1);
-      s.pop_back();
-   }
-}
-int main(){
-   int t;cin>>t;
-   while(t--){
-      cin>>n>>k;
-      letters.clear();
-      for (int i=0;i<n;i++){
-         letters.push_back('A'+i);
-      }
-      vector<char>s;
-      ql(s,0);
-   }
 }
